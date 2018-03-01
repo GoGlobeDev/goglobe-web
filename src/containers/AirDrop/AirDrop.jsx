0018d71e4c70801b77c9488cf9aa80fb7cd9d6d3
zhaoyue <yzhao_130@163.com>
2018-03-01 19:00:13 +0800

    update airdrop

--- src/containers/AirDrop/AirDrop.jsx
+++ src/containers/AirDrop/AirDrop.jsx
@@ -28,11 +28,20 @@ export default class AirDrop extends Component {
                     <p className="air-tip">Enter your ETH address to get 250 free GOG additional 250 GOG for every invited friend</p>
                     <FormGroup>
                         <InputGroup>
-                            <FormControl type="text" value="" className="" />
+                            <FormControl type="text" value="" className="" placeholder="Enter your ETH address " />
                             <InputGroup.Addon>submit</InputGroup.Addon>
                         </InputGroup>
                     </FormGroup>
                 </div>
+                <div className="pos cloud-l1"><img src={require('img/cloud-l1.png')} /></div>
+                <div className="pos cloud-l2"><img src={require('img/cloud-l2.png')} /></div>
+                <div className="pos tree-l"><img src={require('img/tree-l.png')} /></div>
+                <div className="pos hot-ball"><img src={require('img/hot-ball.png')} /></div>
+                <div className="pos cloud-r1"><img src={require('img/cloud-r1.png')} /></div>
+                <div className="pos cloud-r2"><img src={require('img/cloud-r2.png')} /></div>
+                <div className="pos sun"><img src={require('img/sun.png')} /></div>
+                <div className="pos tree-r"><img src={require('img/tree-r.png')} /></div>
+                <div className="pos car"><img src={require('img/car.png')} /></div>
             </div>
         );
     }

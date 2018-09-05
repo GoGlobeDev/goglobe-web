66abfbdfcd4e95009346bd4db4961a2ade74a529
yzhao130 <yzhaoao_130@163.com>
2018-09-05 11:53:16 +0800

    update pagination

--- src/containers/Wallet/Wallet.jsx
+++ src/containers/Wallet/Wallet.jsx
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import Helmet from 'react-helmet';
-import Select from 'rc-select';
+// import Select from 'rc-select';
 import Pagination from 'rc-pagination';
 import 'rc-pagination/assets/index.css';
 import 'rc-select/assets/index.css';
+import moment from 'moment';
+import 'weui';
+import 'react-weui/build/dist/react-weui.css';
+import { InfiniteLoader, LoadMore } from 'react-weui';
+
 
 import { NAV_BAR, LANG, WALLET } from 'theme/Lang';
 import { loadWalletData, loadRecord } from '../Index/indexUtil';
@@ -15,32 +20,72 @@ export default class Wallet extends Component {
     static propTypes = {
         params: PropTypes.object.isRequired
     };
-    state: Object = {
-        activeNav: Number(this.props.params.activeNav),
-        userInfo: {},
-        record: {}
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeNav: Number(this.props.params.activeNav),
+            userInfo: {},
+            record: {},
+            mobileRecord: {}
+        };
+    }
     componentWillMount() {
         const data = {
             ethAddress: this.props.params.searchkey
         };
         loadWalletData(data).then(response => response.json()).then((res) => {
-            console.log(res);
             this.setState({ userInfo: res });
         });
-        loadRecord(data).then(response => response.json()).then((res) => {
-            console.log(res);
-            this.setState({ record: res });
+        const data1 = {
+            ethAddress: this.props.params.searchkey,
+            start: 0,
+            rows: 20
+        };
+        loadRecord(data1).then(response => response.json()).then((result) => {
+            const _mobileRecord = JSON.parse(JSON.stringify(result));
+            _mobileRecord.nextkey = 20;
+            this.setState({ record: result, mobileRecord: _mobileRecord });
         });
     }
     toAnchor: Function = (anchor) => {
         const url = '/' + anchor;
         window.location.href = url;
     }
+    changePage: Function = (pageIndex) => {
+        const data = {
+            ethAddress: this.props.params.searchkey,
+            start: 20 * (pageIndex - 1),
+            rows: 20
+        };
+        loadRecord(data).then(response => response.json()).then((res) => {
+            this.setState({ record: res });
+        });
+    }
+    loadMore: Function = (resolve, finish) => {
+        const _mobileRecord = JSON.parse(JSON.stringify(this.state.mobileRecord));
+        setTimeout(() => {
+            if (_mobileRecord.list.length === _mobileRecord.number) {
+                finish();
+            } else {
+                const data = {
+                    ethAddress: this.props.params.searchkey,
+                    start: _mobileRecord.nextkey,
+                    rows: 20
+                };
+                loadRecord(data).then(response => response.json()).then((res) => {
+                    if (res.list.length > 0) {
+                        _mobileRecord.nextkey = _mobileRecord.nextkey + 20;
+                        _mobileRecord.list = _mobileRecord.list.concat(res.list);
+                        this.setState({ mobileRecord: _mobileRecord });
+                    }
+                }).then(resolve);
+            }
+        }, 1000);
+    }
     render() {
-        const { userInfo, record } = this.state;
+        const { userInfo, record, mobileRecord } = this.state;
         return (
-            userInfo && record.list
+            userInfo && (record.list || mobileRecord.list)
                 ? <div className="wallet">
                     <Helmet title="钱包" />
                     <Navbar collapseOnSelect fixedTop className="header-nav">
@@ -98,12 +143,12 @@ export default class Wallet extends Component {
                                             <div className="head-item">{WALLET.tableTitle.data[LANG]}</div>
                                         </div>
                                         <div className="table-body">
-                                            {[1, 2, 3, 4, 5].map((item) => {
+                                            {record.list.map((item, index) => {
                                                 return (
-                                                    <div key={item} className="body-row">
-                                                        <div className="body-item">{item}</div>
-                                                        <div className="body-item">1253</div>
-                                                        <div className="body-item">2018-8-30</div>
+                                                    <div key={item.id} className="body-row">
+                                                        <div className="body-item">{index + 1}</div>
+                                                        <div className="body-item">{item.amount}</div>
+                                                        <div className="body-item">{moment(item.time).format('YYYY-MM-DD')}</div>
                                                     </div>
                                                 );
                                             })}
@@ -112,13 +157,12 @@ export default class Wallet extends Component {
                                 }
                                 {record.list.length > 0
                                     && <Pagination
-                                        selectComponentClass={Select}
                                         showSizeChanger
                                         showQuickJumper={{ goButton: <button>确定</button> }}
                                         defaultPageSize={20}
                                         defaultCurrent={1}
                                         onShowSizeChange={this.onShowSizeChange}
-                                        onChange={this.onChange}
+                                        onChange={(evt) => this.changePage(evt)}
                                         total={record.number}
                                     />}
                                 {/* <Pagination simple showQuickJumper={{ goButton: true }} defaultCurrent={1} total={50} />
@@ -126,55 +170,62 @@ export default class Wallet extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className="search-result mobile">
-                        <div className="top">
-                            <div className="header">{WALLET.addr[LANG]}：{this.props.params.searchkey}</div>
-                            <div className="info-list">
-                                <div className="info-item">
-                                    <img src={require('img/balance.png')} />
-                                    <p>{WALLET.balance[LANG]}</p>
-                                    <div className="gog"><span className="gog-number">{userInfo.balance || 0}</span> GOG</div>
+                    <div className="mobile">
+                        <InfiniteLoader id="infiniteCon"
+                            onLoadMore={this.loadMore}
+                            loaderLoadingIcon={<LoadMore loading> 加载中... </LoadMore>}
+                            loaderDefaultIcon={<LoadMore showLine> 已经到底啦 </LoadMore>} >
+                            <div className="search-result">
+                                <div className="top">
+                                    <div className="header">{WALLET.addr[LANG]}：{this.props.params.searchkey}</div>
+                                    <div className="info-list">
+                                        <div className="info-item">
+                                            <img src={require('img/balance.png')} />
+                                            <p>{WALLET.balance[LANG]}</p>
+                                            <div className="gog"><span className="gog-number">{userInfo.balance || 0}</span> GOG</div>
+                                        </div>
+                                        <div className="info-item">
+                                            <img src={require('img/balance.png')} />
+                                            <p>{WALLET.device[LANG]}</p>
+                                            <div className="number">{userInfo.deviceSum || 0}</div>
+                                        </div>
+                                        <div className="info-item">
+                                            <img src={require('img/balance.png')} />
+                                            <p>{WALLET.power[LANG]}</p>
+                                            <div className="number">{userInfo.power || 0}</div>
+                                        </div>
+                                        <div className="info-item">
+                                            <img src={require('img/balance.png')} />
+                                            <p>{WALLET.dailyProduce[LANG]}</p>
+                                            <div className="number">{userInfo.dailyProduce || 0}</div>
+                                        </div>
+                                    </div>
+                                    <div className="recomment">{WALLET.recommend[LANG]}：{userInfo.referral || ''}</div>
                                 </div>
-                                <div className="info-item">
-                                    <img src={require('img/balance.png')} />
-                                    <p>{WALLET.device[LANG]}</p>
-                                    <div className="number">{userInfo.deviceSum || 0}</div>
-                                </div>
-                                <div className="info-item">
-                                    <img src={require('img/balance.png')} />
-                                    <p>{WALLET.power[LANG]}</p>
-                                    <div className="number">{userInfo.power || 0}</div>
-                                </div>
-                                <div className="info-item">
-                                    <img src={require('img/balance.png')} />
-                                    <p>{WALLET.dailyProduce[LANG]}</p>
-                                    <div className="number">{userInfo.dailyProduce || 0}</div>
+                                <div className="mining-record">
+                                    <h3 className="table-title">{WALLET.miningRecord[LANG]}</h3>
+                                    {mobileRecord.list.length > 0
+                                        && <div className="mining-table">
+                                            <div className="table-head">
+                                                <div className="head-item"></div>
+                                                <div className="head-item">{WALLET.tableTitle.output[LANG]}</div>
+                                                <div className="head-item">{WALLET.tableTitle.data[LANG]}</div>
+                                            </div>
+                                            <div className="table-body">
+                                                {mobileRecord.list.map((item, index) => {
+                                                    return (
+                                                        <div key={item.id} className="body-row">
+                                                            <div className="body-item">{index + 1}</div>
+                                                            <div className="body-item">{item.amount}</div>
+                                                            <div className="body-item">{moment(item.time).format('YYYY-MM-DD')}</div>
+                                                        </div>
+                                                    );
+                                                })}
+                                            </div>
+                                        </div>}
                                 </div>
                             </div>
-                            <div className="recomment">{WALLET.recommend[LANG]}：{userInfo.referral || ''}</div>
-                        </div>
-                        <div className="mining-record">
-                            <h3 className="table-title">{WALLET.miningRecord[LANG]}</h3>
-                            {record.list.length > 0
-                                && <div className="mining-table">
-                                    <div className="table-head">
-                                        <div className="head-item"></div>
-                                        <div className="head-item">{WALLET.tableTitle.output[LANG]}</div>
-                                        <div className="head-item">{WALLET.tableTitle.data[LANG]}</div>
-                                    </div>
-                                    <div className="table-body">
-                                        {[1, 2, 3, 4, 5].map((item) => {
-                                            return (
-                                                <div key={item} className="body-row">
-                                                    <div className="body-item">{item}</div>
-                                                    <div className="body-item">1253</div>
-                                                    <div className="body-item">2018-8-30</div>
-                                                </div>
-                                            );
-                                        })}
-                                    </div>
-                                </div>}
-                        </div>
+                        </InfiniteLoader>
                     </div>
                 </div> : null
         );
